c9a0f80e85945e9933b1dade1a89319e18f9e4df
emilundg <lundgrenemil@hotmail.com>
2020-08-13 14:07:46 +0200

    ADD structure of login test

--- cypress/integration/user/login.spec.js
+++ cypress/integration/user/login.spec.js
@@ -2,9 +2,10 @@ describe('login', () => {
     beforeEach(() => {
         cy.visit('/login')
     })
-    it('should be working', () => {
-        expect(true)
-            .to
-            .equal(true)
-    })
+    it('should greet with Sign in', () => {})
+    it('should link to register', () => {})
+    it('should require email', () => {})
+    it('should require password', () => {})
+    it('should require valid username and password', () => {})
+    it('should navigate to / on succesful login', () => {})
 })
